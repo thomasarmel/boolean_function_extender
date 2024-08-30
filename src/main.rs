8336890c19a37c211deef297231d4360648f15bc
Thomas Prévost <tprevost@smtp.i3s.unice.fr>
2024-08-30 11:15:26 +0200

    Count the number of functions per equivalence class

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use fast_boolean_anf_transform::fast_bool_anf_transform_unsigned;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use numext_fixed_uint::U512;
 use rayon::prelude::IntoParallelIterator;
 use rayon::iter::ParallelIterator;
@@ -10,29 +11,31 @@ const RING_SIZE: usize = 9;
 const EQUIVALENCE_CLASSES: [u32; 48] = [0xaa55aa55, 0xaa55ab55, 0xaa55bb55, 0xaa5dbb55, 0xaaddbb55, 0xaa5dbb51, 0x2a5dbb51, 0xaaddbb51, 0x2a5dbf51, 0x6a5dbb51, 0x2addbb51, 0xa8ddbb51, 0xaeddda51, 0x0a5dbf51, 0x8addda51, 0xa8dd9b51, 0x88ddbb51, 0x88ddbb11, 0x8c5dda51, 0xa89d9b51, 0x8eddda51, 0xaefdda51, 0x025dbf51, 0x88ddda51, 0x88dd9b51, 0xceddda51, 0x0eddda51, 0x425dbf51, 0x8cddda51, 0x88dddb51, 0x289d9b51, 0x86fdda51, 0x88dddb71, 0xcefdda51, 0x0efdda51, 0x288d9b51, 0x8cfdda51, 0x8cdddb51, 0x8ccdda51, 0x289d9b41, 0x488ddb51, 0xccfdda51, 0x688d9b51, 0x288d9b41, 0x288d1b41, 0xdcfdda51, 0x68ad9b51, 0x688ddb51];
 
 fn main() {
-    /*(0..=u32::MAX).into_par_iter().for_each(|rule_number| {
-        let output_9_rule_number = extend_rule_5_to_9(rule_number);
-        let initial_deg = U32Tester::get_function_degree(&rule_number);
-        if U512Tester::get_function_degree(&output_9_rule_number) == initial_deg {
-            println!("{} -> {} (degree = {})", rule_number, output_9_rule_number, initial_deg);
-        }
-    });*/
-    (0..=u32::MAX).into_par_iter().for_each(|f|  {
-        let walsh_spectrum = U32Tester::absolute_walsh_spectrum(&f);
-        let autocorrelation_spectrum = U32Tester::absolute_autocorrelation_spectrum(&f);
-        let equivalent_classes_count: u32 = EQUIVALENCE_CLASSES.iter().map(|eq| {
-            let walsh_spectrum_eq = U32Tester::absolute_walsh_spectrum(&eq);
-            let autocorrelation_spectrum_eq = U32Tester::absolute_autocorrelation_spectrum(&eq);
-            if walsh_spectrum == walsh_spectrum_eq && autocorrelation_spectrum == autocorrelation_spectrum_eq {
-                1
-            } else {
-                0
+    let mut classes_eq_count: Vec<(u32, HashMap<u32, usize>, HashMap<u32, usize>, Arc<Mutex<usize>>)> = Vec::new();
+
+    for eq in EQUIVALENCE_CLASSES {
+        let walsh_spectrum_eq = U32Tester::absolute_walsh_spectrum(&eq);
+        let autocorrelation_spectrum_eq = U32Tester::absolute_autocorrelation_spectrum(&eq);
+        classes_eq_count.push((eq, walsh_spectrum_eq, autocorrelation_spectrum_eq, Arc::new(Mutex::new(0))));
+    }
+
+    (0..=u32::MAX).into_par_iter().for_each(|rule_number| {
+        let walsh_spectrum = U32Tester::absolute_walsh_spectrum(&rule_number);
+        let autocorrelation_spectrum = U32Tester::absolute_autocorrelation_spectrum(&rule_number);
+        for eq in classes_eq_count.iter() {
+            let walsh_spectrum_eq = &eq.1;
+            let autocorrelation_spectrum_eq = &eq.2;
+            if walsh_spectrum == *walsh_spectrum_eq && autocorrelation_spectrum == *autocorrelation_spectrum_eq {
+                let mut eq_counter = eq.3.lock().unwrap();
+                *eq_counter += 1;
+                break;
             }
-        }).sum();
-        if equivalent_classes_count != 1 {
-            println!("{} -> {}", f, equivalent_classes_count);
         }
     });
+
+    for eq in classes_eq_count {
+        println!("{:#02x} -> {}", eq.0, eq.3.lock().unwrap());
+    }
 }
 
 fn extend_rule_5_to_9(rule_number: u32) -> U512 {
